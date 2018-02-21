38b01152d30bab668e159763f05a980b2482ab39
Programmer <ser.dmitri@gmail.com>
2018-02-21 10:20:09 -0500

    remove semicolons

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const DeltaE = require('delta-e')
 const d3Color = require('d3-color')
 
 const color = argv.color
-console.log("Given color: " + color);
+console.log("Given color: " + color)
 const colors = require('./colors.json')
 
 function hexToLAB(hexString) {
 	const { l, a, b} = d3Color.lab(d3Color.color(hexString))
-	return {L:l, A:a, B:b };
+	return {L:l, A:a, B:b }
 }
 
 function colorSimilarity(fromLAB, toLAB) {
@@ -18,16 +18,16 @@ function colorSimilarity(fromLAB, toLAB) {
 
 function colorsSimilarity(colorLAB) {
 	return (colors) => {
-		const similarities = []; 
+		const similarities = [] 
 		for (const [name, hexValue] of Object.entries(colors)) {
 			const similarity = colorSimilarity(hexToLAB(hexValue), colorLAB)
 			similarities.push({
 				similarity,
 				name,
 				hexValue
-			});
+			})
 		}
-		return similarities;
+		return similarities
 	}
 }
 
